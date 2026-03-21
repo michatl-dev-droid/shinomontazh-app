fa181601621dfd8da2d4b10619628812181d7c63
Michael <9227552@gmail.com>
2026-03-21 20:18:28 +0300

    Fix API URL in Register component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -38,7 +38,7 @@ const Register = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/register', {
+      const response = await axios.post('http://87.249.44.239:5000/api/auth/register', {
         phone: formData.phone,
         password: formData.password,
         name: formData.name,
