6b12e5d845a114321bd121c21657caed3bc9ae0d
Michael <9227552@gmail.com>
2026-03-28 15:49:01 +0300

    Add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
+import Home from './pages/Home';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminCoupons from './pages/admin/AdminCoupons';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
 import PriceList from './pages/PriceList';
 import ServicePoints from './pages/ServicePoints';
 import Map from './pages/Map';
